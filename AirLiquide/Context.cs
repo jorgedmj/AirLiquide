f18f51dd5215f59da805123e0c95ee0c296c6c0a
jorgedmj <jorge.dias@outlook.com>
2020-12-04 19:58:00 -0300

    Adicionar arquivos de projeto.

---
+++ AirLiquide/Context.cs
@@ -0,0 +1,18 @@
+﻿using AirLiquide.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirLiquide
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+        public DbSet<Cliente> Clientes { get; set; }        
+    }
+}
---
+++ AirLiquide/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirLiquide.Models
+{
+    public class Cliente
+    {
+        public Guid Id { get; set; }
+        [StringLength(100)]
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+}
